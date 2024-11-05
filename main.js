fb2c80ee85ae09b4178bc0795a5f2ce3d7748aab
Hungvovan <voh9126@gmail.com>
2024-11-05 15:52:24 +0700

    new project

---
+++ main.js
@@ -0,0 +1,16 @@
+const myDog = {
+  "name":"Pro",
+  "legs":4,
+  tails:1,
+  friends:["shit","Cat"]
+
+  // Only change code below this line
+
+
+  // Only change code above this line
+};
+const animal="name";
+const mycat=myDog[animal];
+
+
+console.log(mycat)
